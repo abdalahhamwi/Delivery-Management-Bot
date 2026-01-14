8b1df90748d32c69845ee0915ff0520e00ec55af
abdala hamwi <abdalah09941@gmail.com>
2026-01-14 15:14:24 +0300

    add tables on database

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Order(models.Model):
+    class OrderStatus(models.TextChoices):
+        PENDING = "PD", "Pending"
+        DELIVERED = "DF", "Delivered"
+
+    adderss = models.CharField(max_length=255)
+    status = models.CharField(max_length=20, choices=OrderStatus,default=OrderStatus.PENDING)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.id
