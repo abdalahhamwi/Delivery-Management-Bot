ba5fb61b3429d6b0b362c586645deb514ccbdaf2
abdala hamwi <abdalah09941@gmail.com>
2026-02-01 12:26:17 +0300

    fix bug

--- project/bot.py
+++ project/bot.py
@@ -8,7 +8,6 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 django.setup()
 
-from typing import Final
 from telegram import Update
 from telegram.ext import (
     ApplicationBuilder,
@@ -23,37 +22,46 @@ from orders.models import Order
 Bot_Token = "8559920620:AAFKAmyWtNkS_yY0cmet_CkfBTUjyKqFROc"
 Bot_UserName = "@Django-Management-Orders"
 
+
 @sync_to_async
 def get_order_by_id(order_id: int) -> str:
     try:
-        order = Order.objects.get(id=order_id) 
+        order = Order.objects.get(id=order_id)
         return (
-        f"Order ID:(order.id)\n",
-        f"Status: (order.get_status_display())\n", 
-        f"Created : (order.created)\n",
-        f"Adderss : (order.adderss)\n",
+            f"Order ID: {order.id}\n"
+            f"Status: {order.get_status_display()}\n"
+            f"Created: {order.created}\n"
+            f"Adderss: {order.adderss}\n"
         )
     except Order.DoesNotExist:
-        return f"order with id (order.id) dose not exist" 
-    
-    
-async def start_command(update: Update, contexts=ContextTypes.DEFAULT_TYPE):
+        return f"Order with ID {order_id} does not exist!"
+
+
+
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     username = update.message.from_user.username
-    await update.message.reply_text(f"Hello {username}! Iam A Management Order Bot. \nPlease Enter Your Order ID to Check its status.")
-    
-    
+    await update.message.reply_text(
+        f"Hello, {username}! I am a Management Order Bot.\n"
+        f"Please enter your Order ID to check its status."
+    )
+
+
 async def handle_order_query(update: Update, context: ContextTypes.DEFAULT_TYPE):
-        text = update.message.text.strip()
-        if text.isdigit():
-            order_id = int(text)
-            response = await get_order_by_id(order_id)
-        else:
-            response = "Invalid input. please send a valid numeric order ID."
-            await update.message.reply_text(response)
-            
-            
-if __name__ == "__main__":
-   app = ApplicationBuilder().token(Bot_Token).build()
-   app.add_handler(CommandHandler("start", start_command))
-   app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_order_query))
-   app.run_polling(poll_interval=3)
+    text = update.message.text.strip()
+
+    if text.isdigit():
+        order_id = int(text)
+        response = await get_order_by_id(order_id)
+    else:
+        response = "Invalid input. Please send a valid numeric order ID."
+
+    await update.message.reply_text(response)
+
+
+if __name__ == '__main__':
+    app = ApplicationBuilder().token(Bot_Token).build()
+
+    app.add_handler(CommandHandler("start", start_command))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_order_query))
+
+    app.run_polling(poll_interval=3)
